7fa7fd2d3477047a417c9b16f62c43036ef856ea
Reaster <darcrai11@gmail.com>
2020-11-12 17:03:28 +0100

    add ft_putendl_fd

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: earnaud <earnaud@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/02 12:57:37 by earnaud           #+#    #+#             */
-/*   Updated: 2020/11/12 16:47:32 by earnaud          ###   ########.fr       */
+/*   Updated: 2020/11/12 17:00:40 by earnaud          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ char	*ft_strrchr(char *str, int c);
 int		ft_strncmp(const char *str1, const char *str2, size_t n);
 size_t	ft_strlcpy(char *dest, const char *src, size_t size);
 void	*ft_memset(void *str, int c, size_t n);
-void	ft_bzero(void *s, size_t n);
+void	ft_bzero(void *, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memccpy(void *dest, const void *src, int c, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
@@ -49,5 +49,5 @@ char	*ft_itoa(int n);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
-
+void	ft_putendl_fd(char *s, int fd);
 #endif
